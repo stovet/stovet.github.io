0a9d416010a4e2a3f77225faf7f215cd1f57713f
stovet <steven.topalian@gmail.com>
2022-02-22 11:26:20 -0500

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+
+function Header(){
+
+    return(
+        <div className="Header">
+            <div className="">
+                <nav>
+                    <ul>
+                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Gallery</a></li>
+
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    )
+};
+
+export default Header;
